a8966d72761eb11db63482c45e1be661568bbc13
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-03-08 13:48:44 -0500

    Interim checkin

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,16 +1,16 @@
-# class MyValidator < ActiveModel::MyValidator
-#   def validate(song)
-#     if song.released && !song.release_year 
-#       # song.errors[:release_year] = “release year is blank”
-#     end
-#   end
-# end
+class MyValidator < ActiveModel::Validator
+  def validate(song)
+    if song.released && !song.release_year 
+      song.errors[:release_year] = "Release year required if released true"
+     end
+   end
+ end
 
 class Song < ActiveRecord::Base
-  # include ActiveModel::Validations
-  # validates_with MyValidator
+   include ActiveModel::Validations
+  validates_with MyValidator
   validates :title, presence: true, uniqueness: true
-  validates :released, inclusion: { in: [true, false]}
-  validates :release_year, presence: true
+  #validates :released, inclusion: { in: [true, false]}
+  validates :release_year, 
 
 end
